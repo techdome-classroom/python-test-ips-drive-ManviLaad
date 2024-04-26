1aef291f4a1cd1a5dcd1dbee3d35864c331ee88f
ManviLaad <134135766+ManviLaad@users.noreply.github.com>
2024-04-26 13:33:07 +0530

    Add files via upload

--- program2.py
+++ program2.py
@@ -1,13 +1,17 @@
 def longest_substring(s: str) -> int:
-    
-    """"
-    
-    Implement the function longest_substring 
-    using the provided longest_substring function to find 
-    the length of the longest substring without repeating characters.
+   max_length=0
+   start=0
+   char_index = {}
+   for i, char in enumerate(s) :
+      if char in char_index and char_index[char] >=start: 
+         start = char_index[char]+1
+   char_index[char]=i
+   max_length = max(max_length,i-start+1)
+   return max_length
 
-    """ 
-    pass
+input_string = "abcabcbb"
+result = longest_substring(input_string)
+print("Length",result)
 
 
 
